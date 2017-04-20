994b6470ab8706ab0879a11e59e93aafed8c6356
Michael Greene <michael.greene@microsoft.com>
2017-04-19 22:41:32 -0500

    fix module name again

--- DSCConfiguration.Tests.ps1
+++ DSCConfiguration.Tests.ps1
@@ -275,7 +275,7 @@ Describe 'Common Tests - File Formatting' -Tag Lint {
 <#
 #>
 Describe 'Common Tests - Configuration Module Requirements' -Tag Unit {
-    $Name = Get-Item -Path $env:BuildFolder\$env:ProjectName+'Module' | ForEach-Object -Process {$_.Name}
+    $Name = $env:ProjectName+'Module'
     $Files = Get-ChildItem -Path $env:BuildFolder\$Name
     $Manifest = Import-PowerShellDataFile -Path "$env:BuildFolder\$Name\$Name.psd1"
 
