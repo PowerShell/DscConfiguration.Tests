f09abaaa00f76a6eeb95d949ba4f03ab7d332210
Michael Greene <michael.greene@microsoft.com>
2018-02-08 17:20:43 -0600

    update tests for ps1

--- DSCConfiguration.Tests.ps1
+++ DSCConfiguration.Tests.ps1
@@ -275,52 +275,47 @@ Describe 'Common Tests - File Formatting' -Tag Lint {
 <#
 #>
 Describe 'Common Tests - Configuration Module Requirements' -Tag Unit {
-    $Files = Get-ChildItem -Path $env:BuildFolder\$env:ProjectName
-    $Manifest = Import-PowerShellDataFile -Path "$env:BuildFolder\$env:ProjectName\$env:ProjectName.psd1"
+    $Files = Get-ChildItem -Path $env:BuildFolder
+    $ScriptFileInfo = Test-ScriptFileInfo -Path "$env:BuildFolder\$env:ProjectName.ps1"
 
-    Context "$env:ProjectName module manifest properties" {
-        It 'Contains a module manifest that aligns to the folder and module names' {
-            $Files.Name.Contains("$env:ProjectName.psd1") | Should Be True
+    Context "$env:ProjectName scriptfileinfo properties" {
+        It 'Contains a script file that aligns to the project name' {
+            $Files.Name.Contains("$env:ProjectName.ps1") | Should Be True
         }
         It 'Contains a readme' {
             $Files.Name.Contains("README.md") | Should Be True
         }
-        It "Manifest $env:ProjectName.psd1 should import as a data file" {
-            $Manifest | Should BeOfType 'Hashtable'
+        It 'Should have a GUID in the scriptfileinfo' {
+            $ScriptFileInfo.GUID | Should Match '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
         }
-        It 'Should have a GUID in the manifest' {
-            $Manifest.GUID | Should Match '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
+        It 'Should list requirements in the scriptfileinfo' {
+            $ScriptFileInfo.RequiredModules | Should Not Be Null
         }
-        It 'Should list requirements in the manifest' {
-            $Manifest.RequiredModules | Should Not Be Null
+        It 'Should list a version in the scriptfileinfo' {
+            $ScriptFileInfo.Version | Should BeGreaterThan 0.0.0
         }
-        It 'Should list a module version in the manifest' {
-            $Manifest.ModuleVersion | Should BeGreaterThan 0.0.0.0
+        It 'Should list an author in the scriptfileinfo' {
+            $ScriptFileInfo.Author | Should Not Be Null
         }
-        It 'Should list an author in the manifest' {
-            $Manifest.Author | Should Not Be Null
+        It 'Should provide a description in the scriptfileinfo' {
+            $ScriptFileInfo.Description | Should Not Be Null
         }
-        It 'Should provide a description in the manifest' {
-            $Manifest.Description | Should Not Be Null
+        It 'Should include tags in the scriptfileinfo' {
+            $ScriptFileInfo.Tags | Should Not Be Null
         }
-        It 'Should require PowerShell version 4 or later in the manifest' {
-            $Manifest.PowerShellVersion | Should BeGreaterThan 4.0
+        It 'Should include a project URI in the scriptfileinfo' {
+            $ScriptFileInfo.ProjectURI | Should Not Be Null
         }
-        It 'Should require CLR version 4 or later in the manifest' {
-            $Manifest.CLRVersion | Should BeGreaterThan 4.0
+        It 'Should include a license URI in the scriptfileinfo' {
+            $ScriptFileInfo.LicenseURI | Should Not Be Null
         }
-        It 'Should export functions in the manifest' {
-            $Manifest.FunctionsToExport | Should Not Be Null
-        }
-        It 'Should include tags in the manifest' {
-            $Manifest.PrivateData.PSData.Tags | Should Not Be Null
-        }
-        It 'Should include a project URI in the manifest' {
-            $Manifest.PrivateData.PSData.ProjectURI | Should Not Be Null
+        It 'Should include release notes in the scriptfileinfo' {
+            $ScriptFileInfo.ReleaseNotes | Should Not Be Null
         }
     }
+    <#
     Context "$env:ProjectName required modules" {
-        ForEach ($RequiredModule in $Manifest.RequiredModules[0]) {
+        ForEach ($RequiredModule in $ScriptFileInfo.RequiredModules.split(',')) {
             if ($RequiredModule.GetType().Name -eq 'Hashtable') {
                 It "$($RequiredModule.ModuleName) version $($RequiredModule.ModuleVersion) should be found in the PowerShell public gallery" {
                     {Find-Module -Name $RequiredModule.ModuleName -RequiredVersion $RequiredModule.ModuleVersion} | Should Not Be Null
@@ -339,11 +334,7 @@ Describe 'Common Tests - Configuration Module Requirements' -Tag Unit {
             }
         }
     }
-    Context "$env:ProjectName module" {
-        It "$env:ProjectName imports as a module" {
-            {Import-Module -Name $env:ProjectName} | Should Not Throw
-        }
-    }
+    #>
     Context "$env:ProjectName configuration script" {
         It "$env:BuildFolder\$env:ProjectName.ps1 should execute as a script without error" {
             {. $env:BuildFolder\$env:ProjectName.ps1} | Should Not Throw
