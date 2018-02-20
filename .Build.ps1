6bc52517c7e47fae53f7666ae5d7c14ba52bc0b7
Michael Greene <michael.greene@microsoft.com>
2018-02-19 22:20:49 -0600

    auto cleanup

--- .Build.ps1
+++ .Build.ps1
@@ -237,7 +237,7 @@ Add-BuildTask IntegrationTestAzureVMs {
 .Synopsis: remove all assets deployed to Azure and any local temporary changes (should be none)
 #>
 Exit-Build {
-    #Remove-AzureTestResources
+    Remove-AzureTestResources
 }
 
 <#
